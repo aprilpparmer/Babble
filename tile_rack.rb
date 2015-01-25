5aedf9cbe3da550a03d5e065de97f2d8f19d2a6b
aparmer1 <aparmer1@my.westga.edu>
2015-01-24 18:53:57 -0800

    Added number_of_tiles_needed and has_tiles_for? methods. Still need to fix the latter.

--- tile_rack.rb
+++ tile_rack.rb
@@ -1,7 +1,23 @@
-class TileRack < TileGroup
-  def number_of_tiles_needed
-  
-  end
-  end
-  
+require_relative 'tile_group.rb'
+
+class TileRack
+include TileGroup
+
+def get_tiles
+  @@tiles
+end
+
+def number_of_tiles_needed
+	t = @@tiles.length
+	needed = 7 - t
+	return needed
+end
+
+def has_tiles_for?(text)
+	text_array = text.split(//)
+	text_array = text_array.map &:to_sym
+	word_check = @@tiles
+	word_check.eql?(text_array)
+end
+end
 
