81d127bb4ac64bed60735ae61e13a69a3d7bf897
aparmer1 <aparmer1@my.westga.edu>
2015-01-25 08:19:04 -0800

    Finished Word class

--- word.rb
+++ word.rb
@@ -1,9 +1,13 @@
 require_relative 'tile_group.rb'
+require_relative 'tile_bag.rb'
 
 class Word
-include TileGroup
 
    def score
-   	   
+   	   score = 0
+   	   @@tiles.each do |x|
+   	   	   score = score + TileBag.points_for(x)
+   	   end
+   	return score   
    end
 end
